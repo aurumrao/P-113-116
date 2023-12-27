3f66098fe3eda8af7f2624d13d668e8e08e61c13
aurumrao <109160817+aurumrao@users.noreply.github.com>
2023-12-27 11:01:25 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-function preload(){
+function preload() {
 
 }
-function setup(){
+
+function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
-function draw(){
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("Nose  X = " + results[0].pose.nose.x);
+        console.log("Nose  Y = " + results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
 
 }
-function savePic(){
-    save('mewithamustache.png')
+
+function draw() {
+image(video, 0, 0, 300, 300);
+}
+
+function save_pic() {
+    save('mewithamustache.png');
 }
